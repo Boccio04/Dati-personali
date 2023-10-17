ced8a9c68f2335dd21ce40539362ee29211e2b6b
Boccio04 <alessandrobocedi2004@gmail.com>
2023-10-17 11:33:07 +0200

    prima parte

---
+++ testperson/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace TestPerson
+{
+	[TestClass]
+	public class TestPerson
+	{
+		[TestMethod]
+		public void TestConstructors()
+		{
+            //declare variable
+            personClass.Person myPerson;
+
+			//test parametless constructor
+			myPerson = new personClass.Person();
+
+			//test constructor
+			myPerson = new personClass.Person("Mario", "Rossi");
+
+			//test constructor
+			myPerson = new personClass.Person("Mario", "Rossi", personClass.Person.GenderType.Male);
+
+			//test constructor
+			myPerson = new personClass.Person("Mario", "Rossi", personClass.Person.GenderType.Male, "Roma", new DateTime(2000, 10, 15));
+		}
+	}
+}
